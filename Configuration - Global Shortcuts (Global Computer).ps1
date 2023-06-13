dbddd656d1fe995af6edfa72c4fab70f7eb362cf
jonathansaboury <130409862+jonathansaboury@users.noreply.github.com>
2023-06-13 12:48:52 -0700

    Update Configuration - Global Shortcuts (Global Computer).ps1

--- Configuration - Global Shortcuts (Global Computer).ps1
+++ Configuration - Global Shortcuts (Global Computer).ps1
@@ -5,15 +5,12 @@ if (!(Test-Path -Path 'C:\Temp')){
 else{}
 
 #Step 2 - Download ICO files into previous created folder if they don't exist. *If a new image is wanted, we will incriment the names. I.E. adp2.ico.
-if (!(Test-Path 'C:\Temp\ukg.ico' -PathType Leaf))
-{
+if (!(Test-Path 'C:\Temp\ukg.ico' -PathType Leaf)){
 	Invoke-WebRequest -Uri 'https://stnormreevespublic001.blob.core.windows.net/intune/ukg.ico' -Outfile c:\temp\ukg.ico
 }
 else{}
 
-
 #Step 3 - Add the custom URL shortcut to your Desktop with custom icon. Recreating the public desktop icons is by design. If a new image is uploaded this will be set to change. Only disk IO so not an issue.
-
 $newObject = New-Object -ComObject WScript.Shell
 $destination = $newObject.SpecialFolders.Item('AllUsersDesktop')
 $sourcePath = Join-Path -Path $destination -ChildPath '\\UKG.lnk'
